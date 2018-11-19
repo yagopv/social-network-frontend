18e84caaf440536abfee143cc9a19ad820141d1e
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-11-19 09:39:24 +0100

    [ADD] Header and Footer components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,24 @@ import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from 'app/shared/components/not-found/not-found.component';
 import { GridLayoutProjectionComponent } from './components/grid-layout-projection/grid-layout-projection.component';
 import { GridLayoutTemplateComponent } from './components/grid-layout-template/grid-layout-template.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [NotFoundComponent, GridLayoutTemplateComponent, GridLayoutProjectionComponent],
-  exports: [NotFoundComponent, GridLayoutTemplateComponent, GridLayoutProjectionComponent]
+  declarations: [
+    NotFoundComponent,
+    GridLayoutTemplateComponent,
+    GridLayoutProjectionComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    NotFoundComponent,
+    GridLayoutTemplateComponent,
+    GridLayoutProjectionComponent,
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class SharedModule {}
