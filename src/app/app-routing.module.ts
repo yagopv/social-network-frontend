ee30e3083c8fa90417af56931bfa03e2ea99baf8
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-11-21 23:27:34 +0100

    [UPDATE] Routeds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from 'app/shared/components/not-found/not-found.component';
+import { WelcomeComponent } from './welcome/welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
