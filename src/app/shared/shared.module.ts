882d043e39d59b0cdcb43527595ffeccd8857560
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-11-21 21:51:07 +0100

    [REMOVE] Too complex components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,32 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from 'app/shared/components/not-found/not-found.component';
-import { GridLayoutProjectionComponent } from './components/grid-layout-projection/grid-layout-projection.component';
-import { GridLayoutTemplateComponent } from './components/grid-layout-template/grid-layout-template.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { FormWrapperComponent } from './components/form-wrapper/form-wrapper.component';
-import { FormGroupComponent } from './components/form-group/form-group.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [
-    NotFoundComponent,
-    GridLayoutTemplateComponent,
-    GridLayoutProjectionComponent,
-    HeaderComponent,
-    FooterComponent,
-    FormWrapperComponent,
-    FormGroupComponent
-  ],
-  exports: [
-    NotFoundComponent,
-    GridLayoutTemplateComponent,
-    GridLayoutProjectionComponent,
-    HeaderComponent,
-    FooterComponent,
-    FormWrapperComponent,
-    FormGroupComponent
-  ]
+  declarations: [NotFoundComponent, FormWrapperComponent],
+  exports: [NotFoundComponent, FormWrapperComponent]
 })
 export class SharedModule {}
