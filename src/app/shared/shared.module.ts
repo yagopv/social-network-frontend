138d884698574deddbe4f826f6b1ea24f6b21bbc
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-11-26 17:59:58 +0100

    [ADD] Home Dashboard

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from 'app/shared/components/not-found/not-found.component';
 import { FormWrapperComponent } from 'app/shared/components/form-wrapper/form-wrapper.component';
 import { ContainerComponent } from './components/container/container.component';
+import { PictureComponent } from './components/picture/picture.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [NotFoundComponent, FormWrapperComponent, ContainerComponent],
-  exports: [NotFoundComponent, FormWrapperComponent, ContainerComponent]
+  declarations: [
+    NotFoundComponent,
+    FormWrapperComponent,
+    ContainerComponent,
+    PictureComponent
+  ],
+  exports: [
+    NotFoundComponent,
+    FormWrapperComponent,
+    ContainerComponent,
+    PictureComponent
+  ]
 })
 export class SharedModule {}
