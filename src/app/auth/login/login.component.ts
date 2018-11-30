62e5ba1255ca003f0c0f25852af9fe0efc388277
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-11-30 13:29:36 +0100

    [ADD] Mark inputs as touched

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormControl, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,14 +8,22 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   loginModel = {};
-
   constructor(private router: Router) {}
 
   login(form: NgForm) {
-    if (form.valid) {
-      // TODO: Login user
-      console.log('login() - ', form.value);
-      this.router.navigate(['/dashboard']);
+    if (!form.valid) {
+      this.markControlsAsTouched(form.controls);
+      return;
     }
+
+    // TODO: Login user
+    console.log('login() - ', form.value);
+    this.router.navigate(['/dashboard']);
+  }
+
+  private markControlsAsTouched(controls: { [key: string]: AbstractControl }) {
+    Object.values(controls).forEach(control => {
+      control.markAsTouched();
+    });
   }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { MailValidator } from 'app/shared/validators/mail.validator';
 import { Router } from '@angular/router';
 
@@ -19,11 +19,20 @@ export class RegisterComponent {
   }
 
   register() {
-    if (this.registerForm.valid) {
-      const formData = this.registerForm.value;
-      // TODO: Register user
-      console.log('register() - ', formData);
-      this.router.navigate(['/dashboard']);
+    if (!this.registerForm.valid) {
+      this.markFormGroupTouched(this.registerForm);
+      return;
     }
+
+    const formData = this.registerForm.value;
+    // TODO: Register user
+    console.log('register() - ', formData);
+    this.router.navigate(['/dashboard']);
+  }
+
+  private markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+    });
   }
 }
