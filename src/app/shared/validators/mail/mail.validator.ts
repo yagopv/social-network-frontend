f2b44558c2db9863c2b09cc09e8544e55789acfd
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-12-22 19:36:30 +0100

    [ADD] Button styles

--- src/app/shared/validators/mail/mail.validator.ts
+++ src/app/shared/validators/mail/mail.validator.ts
@@ -7,7 +7,7 @@ export function MailValidator(
   const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
   if (
-    control.value !== '' &&
+    control.value &&
     (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))
   ) {
     return { malformedMail: true };
