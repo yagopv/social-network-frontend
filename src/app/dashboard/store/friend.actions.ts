1f038c88a737c61265d1c5b183c5fb6a25d26d86
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-02-03 21:40:51 +0100

    [ADD] Get Friend request actions

--- src/app/dashboard/store/friend.actions.ts
+++ src/app/dashboard/store/friend.actions.ts
@@ -13,3 +13,16 @@ export class SearchUsersFailed {
   static readonly type = '[Friend] SearchUsersFailed';
   constructor(public errors: Error[]) {}
 }
+
+export class GetFriendRequests {
+  static readonly type = '[Friend] GetFriendRequests';
+  constructor() {}
+}
+export class GetFriendRequestsSuccess {
+  static readonly type = '[Friend] GetFriendRequestsSuccess';
+  constructor(public friendsRequests: any[]) {}
+}
+export class GetFriendRequestFailed {
+  static readonly type = '[Friend] GetFriendRequestFailed';
+  constructor(public errors: Error[]) {}
+}
