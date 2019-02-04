c938aadd42cf3594378aaff56559e252189b10d7
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-02-04 19:26:22 +0100

    [UPDATE] Improve styles

--- src/app/dashboard/components/post-content/post-content.component.ts
+++ src/app/dashboard/components/post-content/post-content.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, AfterViewInit, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  AfterViewInit,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
 import { Post } from '../../models/post.model';
 import { markViewDirty } from '@angular/core/src/render3/instructions';
 import marked from 'marked';
@@ -6,7 +12,8 @@ import marked from 'marked';
 @Component({
   selector: 'sn-post-content',
   templateUrl: './post-content.component.html',
-  styleUrls: ['./post-content.component.scss']
+  styleUrls: ['./post-content.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PostContentComponent implements OnInit {
   @Input() post: Post;
