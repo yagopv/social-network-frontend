0fd75415ba541b411d66d75ce4fd4b0bbb3a7e30
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-02-05 19:24:38 +0100

    [ADD] Navigation in Authh routes

--- src/app/auth/containers/login/login.component.ts
+++ src/app/auth/containers/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgForm, AbstractControl } from '@angular/forms';
 
