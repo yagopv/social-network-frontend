00bfd67b225195e6f59b91321041257f8f58b34a
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-02-23 00:29:05 +0100

    [UPDATE] About

--- src/app/about/components/about/about.component.ts
+++ src/app/about/components/about/about.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'sn-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class AboutComponent {
+  title = 'Hey there!! 👋';
+  subtitle = 'My name is Yago';
+  description = `I'm a frontend developer and love to create beautiful websites !!. You can check more about me in the followings sites:`;
+  links = [
+    { url: 'http://linkedin.com/yagopv', name: 'LinkedIn' },
+    { url: 'http://github.com/yagopv', name: 'Github' },
+    { url: 'http://portfolio.yagoperez.com', name: 'Portfolio' },
+    { url: 'http://twitter.com/yperezva', name: 'Twitter' }
+  ];
+  contact = {
+    description: 'If you want to contact with me you can send me an email to:',
+    mail: 'yagoperez@outlook.com'
+  };
 }
