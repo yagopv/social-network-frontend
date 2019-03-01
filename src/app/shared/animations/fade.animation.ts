f174df691e674d796d9781ddc60ae3585c46889c
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-03-01 15:19:07 +0100

    [ADD] Animations

--- src/app/shared/animations/fade.animation.ts
+++ src/app/shared/animations/fade.animation.ts
@@ -3,7 +3,7 @@ import { transition, trigger, animate, style } from '@angular/animations';
 export const FADE_IN_OUT_ANIMATION = trigger('fade', [
   transition('* => in', [
     style({ opacity: 0 }),
-    animate(1000, style({ opacity: 1 }))
+    animate(500, style({ opacity: 1 }))
   ]),
-  transition('* => out', [animate(1000, style({ opacity: 0 }))])
+  transition('* => out', [animate(500, style({ opacity: 0 }))])
 ]);
