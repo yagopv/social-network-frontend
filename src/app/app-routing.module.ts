8401cd83cecdf6e25e2caa243c8dcebb78cbc6bc
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-03-13 11:38:04 +0100

    [FIX] Lazy loading problem with about module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,12 +17,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    canActivate: [AuthGuard],
-    loadChildren: './dashboard/dashboard.module#DashboardModule'
+    loadChildren: './about/about.module#AboutModule'
   },
   {
     path: '',
-    loadChildren: './about/about.module#AboutModule'
+    canActivate: [AuthGuard],
+    loadChildren: './dashboard/dashboard.module#DashboardModule'
   },
   {
     path: 'notification/:notificationName',
