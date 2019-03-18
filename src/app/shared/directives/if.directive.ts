44684dfdccca06cc2c49b87bc71ea715c46b71ba
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-03-18 15:08:51 +0100

    [UPDATE] If directive

--- src/app/shared/directives/if.directive.ts
+++ src/app/shared/directives/if.directive.ts
@@ -3,24 +3,37 @@ import {
   Input,
   ElementRef,
   TemplateRef,
-  ViewContainerRef
+  ViewContainerRef,
+  OnChanges,
+  SimpleChange,
+  SimpleChanges
 } from '@angular/core';
 
 @Directive({
   selector: '[snIf]'
 })
-export class IfDirective {
+export class IfDirective implements OnChanges {
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainer: ViewContainerRef
   ) {}
 
-  @Input()
-  set snIf(condition: boolean) {
-    if (condition) {
+  @Input() snIf;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.snIf.currentValue) {
       this.viewContainer.createEmbeddedView(this.templateRef);
     } else {
       this.viewContainer.clear();
     }
   }
+
+  // @Input()
+  // set snIf(condition: boolean) {
+  //   if (condition) {
+  //     this.viewContainer.createEmbeddedView(this.templateRef);
+  //   } else {
+  //     this.viewContainer.clear();
+  //   }
+  // }
 }
