3f3d1623be7d7fc75813aec1f303fd219985756d
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-04-15 13:12:17 +0200

    [ADD] Beautiful test

--- src/app/core/services/toast.service.ts
+++ src/app/core/services/toast.service.ts
@@ -26,13 +26,30 @@ export class ToastService {
   addErrorToast(error: Error, delay?: number) {
     this.toasts.unshift({
       title: 'Error',
-      message: error.detail,
+      message: this.getErrorMessage(error),
       variant: TOAST.ERROR
     });
 
     if (delay) {
       this.delayAndRemove(delay);
     }
+  }
+
+  getErrorMessage({ detail, data }: Error): string {
+    if (detail) {
+      return detail;
+    }
+
+    if (data) {
+      return `You ${data.label} is wrong`;
+    }
+  }
+
+  isErrorToast(toast: Toast) {
+    if (toast.variant === TOAST.ERROR) {
+      return true;
+    }
+    return false;
   }
 
   delayAndRemove(milliseconds: number) {
--- src/app/features/login/login/login.component.ts
+++ src/app/features/login/login/login.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  Validators,
+  FormGroup,
+  FormControl,
+  AbstractControl
+} from '@angular/forms';
 
 import { MailValidator } from '../../../shared/validators/mail.validator';
 import { AuthStore } from '../../../core/store/auth.store';
 import { UserStore } from '../../../core/store/user.store';
 import { ActivatedRoute, Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'sn-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   loginForm = this.fb.group(
     {
       email: ['', [Validators.required, MailValidator]],
@@ -27,23 +34,25 @@ export class LoginComponent implements OnInit {
     private route: ActivatedRoute
   ) {}
 
-  ngOnInit() {
-    // this.actions$.pipe(ofAction(LoginFailed)).subscribe(() => {
-    //   this.loginForm.get('password').reset();
-    // });
-  }
-
   login() {
     if (!this.loginForm.valid) {
       this.markFormGroupTouched(this.loginForm);
       return;
     }
 
-    this.authStore.login(this.loginForm.value).subscribe(() => {
-      const returnUrl = this.route.queryParams['return-url'];
-      this.router.navigate([returnUrl || '/wall']);
-      this.userStore.getProfile().subscribe();
-    });
+    this.authStore
+      .login(this.loginForm.value)
+      .pipe(
+        catchError(error => {
+          this.loginForm.get('password').setValue('');
+          return error;
+        })
+      )
+      .subscribe(() => {
+        const returnUrl = this.route.queryParams['return-url'];
+        this.router.navigate([returnUrl || '/wall']);
+        this.userStore.getProfile().subscribe();
+      });
   }
 
   private markFormGroupTouched(formGroup: FormGroup) {
