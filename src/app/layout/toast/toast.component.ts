2efa87b812069eb589d09b177861410719b3ded3
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-04-16 17:58:51 +0200

    [ADD] Toast Animation

--- src/app/layout/toast/toast.component.ts
+++ src/app/layout/toast/toast.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ToastService } from '../../core/services/toast.service';
+import { LIST_ITEMS_ANIMATION } from '../../shared/animations/list.animation';
 
 @Component({
   selector: 'sn-toast',
   templateUrl: './toast.component.html',
-  styleUrls: ['./toast.component.scss']
+  styleUrls: ['./toast.component.scss'],
+  animations: [LIST_ITEMS_ANIMATION]
 })
 export class ToastComponent {
   constructor(public toastService: ToastService) {}
