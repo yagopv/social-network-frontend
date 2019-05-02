3d118ea00ef6605be304a927d70306956a1172e4
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-05-02 15:11:56 +0200

    [FIX] Some fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { environment } from '../environments/environment';
 import { LoaderStore } from './core/store/loader.store';
 import { startWith, delay } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'sn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = environment.siteName;
-
   isLoading$: Observable<boolean>;
+  @HostBinding('id') routeId;
 
-  constructor(public loaderStore: LoaderStore) {
+  constructor(public loaderStore: LoaderStore, private router: Router) {
     this.isLoading$ = this.loaderStore.state$.pipe(
       startWith(false),
       delay(0)
     );
+  }
+
+  ngOnInit() {
+    // Set a general id in order to use host-context
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.routeId = event.url.substring(1);
+      }
+    });
   }
 }
