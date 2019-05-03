0f4e93a5cdbf78f9d4f905b12637811f318f2d96
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-05-03 23:38:35 +0200

    [UPDATE] Rename variable

--- src/app/features/welcome/welcome/welcome.component.ts
+++ src/app/features/welcome/welcome/welcome.component.ts
@@ -14,7 +14,7 @@ import { Hero } from '../welcome.models';
   ]
 })
 export class WelcomeComponent {
-  rows: Hero[] = [
+  heroItems: Hero[] = [
     { id: 1, icon: 'fa fa-search', message: 'Find people like you' },
     { id: 2, icon: 'fa fa-users', message: 'Build you friends network' },
     { id: 3, icon: 'fa fa-comments', message: 'Keep in touch' }
